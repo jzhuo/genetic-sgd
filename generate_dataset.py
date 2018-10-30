3765edb144504294d7079c7c9fbdaec36f557dca
Jack Kenney <kenney.jnk@gmail.com>
2018-10-30 11:40:32 -0400

    more definitions

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,3 +1,22 @@
 """
 Library to generate the datasets for training and testing on.
+
+Contains functions that:
+* generate N input points X in D space
+* calculate N output points Y of function F
+* function F can be chosen from global list of functions
 """
+
+
+def generate_inputs(shape):
+    """Shape is (N,D)."""
+    pass
+
+
+def calculate_outputs(inputs, func):
+    """Given (N,D) input, create (N,) outputs."""
+    pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+This is the main functtion for the files.
+
+It should contain functions that
+* Load the dataset or datasets to be used
+* Initialize the genetic algorithm and its population
+* Train on the dataset
+"""
+
+
+def load_dataset(name):
+    pass
+
+
+def initiallize_ga(args):
+    pass
+
+
+def initiallize_one_nn(args):
+    pass
+
+
+def train_on_dataset(estimator):
+    pass
+
+
+if __name__ == "__main__":
+    pass
