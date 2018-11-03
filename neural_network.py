ff40e68feef7868837843c14a3eedb81abcf19e6
Jack Kenney <kenney.jnk@gmail.com>
2018-11-03 14:27:51 -0400

    reformatted nn

--- neural_network.py
+++ neural_network.py
@@ -11,41 +11,23 @@ from keras import optimizers
 
 
 def build_NN(input_size, hidden_layer_size, output_size, learning_rate):
-    # TODO: 
-    # run through the layers, 
-    # compile the model
-    # get weights
-    # define weights
+    """Build single hidden layer network in Keras and return it."""
     model = Sequential()
-    model.add(
-        Dense(
-            input_size,
-            Activation=None,
-            bias_initializer=constant(1),
-        )
-    )
+    model.add(Dense(input_size, Activation=None, bias_initializer=constant(1)))
     model.add(
         Dense(
             hidden_layer_size,
             input_dim=input_size,
-            Activation='tanh',
+            Activation="tanh",
             bias_initializer=constant(1),
         )
     )
-    model.add(
-        Dense(
-            output_size,
-            input_dim=hidden_layer_size,
-            Activation=None,
-        )
-    )
-    optimizer = optimizers.SGD(
-        lr=learning_rate,
-    )
+    model.add(Dense(output_size, input_dim=hidden_layer_size, Activation=None))
+    optimizer = optimizers.SGD(lr=learning_rate)
 
-    model.compile(optimizer=optimizer, loss='mse')
+    model.compile(optimizer=optimizer, loss="mse")
     return model
+
 
 def get_weights(model):
     return model.get_weights()
-    
