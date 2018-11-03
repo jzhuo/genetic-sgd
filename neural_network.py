1fe846b43396348d7da6a69f222210d2892acef2
Jack Kenney <kenney.jnk@gmail.com>
2018-11-03 15:24:22 -0400

    reformatting

--- neural_network.py
+++ neural_network.py
@@ -10,32 +10,22 @@ from keras.initializers import constant
 from keras import optimizers
 
 
-def build_NN(input_size, hidden_layer_size, output_size, learning_rate, weights=None):
+def build_NN(
+    input_size, hidden_layer_size, output_size, learning_rate, weights=None
+):
     """Build single hidden layer network in Keras and return it."""
-    
+
     model = Sequential()
-    model.add(
-        Dense(
-            input_size,
-            Activation=None,
-            bias_initializer=constant(1),
-        )
-    )
+    model.add(Dense(input_size, Activation=None, bias_initializer=constant(1)))
     model.add(
         Dense(
             hidden_layer_size,
             input_dim=input_size,
-            Activation='tanh',
+            Activation="tanh",
             bias_initializer=constant(1),
         )
     )
-    model.add(
-        Dense(
-            output_size, 
-            input_dim=hidden_layer_size, 
-            Activation=None
-        )
-    )
+    model.add(Dense(output_size, input_dim=hidden_layer_size, Activation=None))
     optimizer = optimizers.SGD(lr=learning_rate)
 
     if weights is not None:
